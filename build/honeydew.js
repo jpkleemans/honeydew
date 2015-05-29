74e62597728ee57a6c032049befa3828fabdbbd4
Michael Bakker <michael.bakker@finan.nl>
2015-05-29 15:06:33 +0200

    Moving into interactive version

--- build/honeydew.js
+++ build/honeydew.js
@@ -1,60 +1,3 @@
-function VariableRepository() {
-    this.findByKey = function (key) {
-        var observers = {};
-
-        return {
-            observe: function (event, callback) {
-                if (observers[event] === undefined) {
-                    observers[event] = [];
-                }
-
-                observers[event].push(callback);
-            },
-            setAttributes: function (attributes) {
-                var i = observers['change:attributes'].length;
-                while (i--) {
-                    observers['change:attributes'][i]();
-                }
-            },
-            getAttributes: function () {
-                return {
-                    value: 10,
-                    style: "color:red;"
-                };
-            },
-            getKey: function () {
-                return "";
-            },
-            getTitle: function () {
-                return "";
-            },
-            getChildren: function () {
-                return [];
-            },
-            getContext: function (query) {
-                return {
-                    observe: function (event, callback) {
-                        //
-                    },
-                    setAttributes: function (attributes) {
-                        //
-                    },
-                    getAttributes: function () {
-                        return {};
-                    },
-                    getKey: function () {
-                        return "";
-                    },
-                    getTitle: function () {
-                        return "";
-                    }
-                };
-            }
-        };
-    };
-}
-
-
 var Honeydew;
 (function (Honeydew) {
     var UIContext = (function () {
@@ -68,9 +11,8 @@ var Honeydew;
 var Honeydew;
 (function (Honeydew) {
     var UIVariable = (function () {
-        function UIVariable(key, title, attributes, children) {
+        function UIVariable(key, title, children, attributes) {
             if (attributes === void 0) { attributes = {}; }
-            if (children === void 0) { children = []; }
             this.key = key;
             this.title = title;
             this.attributes = attributes;
@@ -81,28 +23,38 @@ var Honeydew;
     Honeydew.UIVariable = UIVariable;
 })(Honeydew || (Honeydew = {}));
 /// <reference path="../type_definitions/angularjs/angular.d.ts" />
+/// <reference path="../type_definitions/fes/fes.d.ts" />
 var Honeydew;
 (function (Honeydew) {
     var FesBindAttributes = (function () {
         function FesBindAttributes($compile, variables) {
             var _this = this;
+            this.scope = {};
+            this.terminal = true;
             this.$compile = $compile;
             this.variables = variables;
             this.link = function (scope, element, attrs) {
                 var variableKey = attrs['fesBindAttributes'];
                 var variable = _this.variables.findByKey(variableKey);
                 scope[variableKey] = _this.getUIVariable(variable);
-                variable.observe('change:attributes', function () {
-                    console.log('fired!');
-                });
-                //var variableKey:string = attrs['fesBindAttributes'];
                 if (scope[variableKey] === undefined) {
                     throw new Error('Variable ' + variableKey + ' not initialized');
                 }
+                /*
+                variable.observe('change:attributes', (newValue) =>
+                {
+                    console.log(newValue);
+                });
+
+                 scope.$watch(variableKey + '.attributes', function (newAttrs)
+                 {
+                 variable.setAttributes(newAttrs);
+                 }, true);
+
+                //var variableKey:string = attrs['fesBindAttributes'];
+
+                */
                 var attributes = scope[variableKey].attributes;
-                scope.$watch(variableKey + '.attributes', function (newAttrs) {
-                    variable.setAttributes(newAttrs);
-                }, true);
                 _this.setAttributes(variableKey, attributes, element);
                 element.removeAttr('fes-bind-attributes');
                 _this.$compile(element)(scope);
@@ -141,7 +93,12 @@ var Honeydew;
             var key = variable.getKey();
             var title = variable.getTitle();
             var attributes = variable.getAttributes();
-            var UIVariable = new Honeydew.UIVariable(key, title, attributes);
+            var children = [];
+            var i = variable.getChildren().length;
+            while (i--) {
+                children.push(this.getUIVariable(variable.getChildren()[i]));
+            }
+            var UIVariable = new Honeydew.UIVariable(key, title, children, attributes);
             return UIVariable;
         };
         return FesBindAttributes;
@@ -162,33 +119,38 @@ var Honeydew;
                 scope[variableKey] = _this.getUIVariable(variable);
             };
         }
-        ///**
-        // *
-        // * @param scope
-        // * @param element
-        // * @param attrs
-        // */
-        //public link(scope:angular.IScope, element:angular.IAugmentedJQuery, attrs:angular.IAttributes):void
-        //{
-        //    var variableKey:string = attrs['fesInit'];
-        //    var variable:Fes.IVariable = this.variables.findByKey(variableKey);
-        //
-        //    scope['variable'] = this.getUIVariable(variable);
-        //}
-        /**
-         *
-         * @param variable
-         * @returns {Honeydew.UIVariable}
-         */
         FesInit.prototype.getUIVariable = function (variable) {
             var key = variable.getKey();
             var title = variable.getTitle();
-            var UIVariable = new Honeydew.UIVariable(key, title);
+            var children = variable.getChildren();
+            var UIVariable = new Honeydew.UIVariable(key, title, children);
             return UIVariable;
         };
         return FesInit;
     })();
     Honeydew.FesInit = FesInit;
+})(Honeydew || (Honeydew = {}));
+/// <reference path="../type_definitions/angularjs/angular.d.ts" />
+/// <reference path="../type_definitions/fes/fes.d.ts" />
+var Honeydew;
+(function (Honeydew) {
+    var FesRepeat = (function () {
+        function FesRepeat($compile, variables) {
+            var _this = this;
+            this.priority = 1005;
+            this.$compile = $compile;
+            this.variables = variables;
+            this.link = function (scope, element, attrs) {
+                var repeat = attrs['ngRepeat'];
+                var repeatvariable = repeat.match('in (.*)$')[1];
+                var variable = _this.variables.findByKey('ROOT');
+                scope['ROOT'] = variable;
+                console.log(scope['ROOT']);
+            };
+        }
+        return FesRepeat;
+    })();
+    Honeydew.FesRepeat = FesRepeat;
 })(Honeydew || (Honeydew = {}));
 /// <reference path="type_definitions/angularjs/angular.d.ts" />
 var Honeydew;
@@ -214,12 +176,14 @@ var Honeydew;
             directive['$inject'] = ['$compile', 'IVariableRepository'];
             return directive;
         };
+        DirectiveFactory.FesRepeat = function () {
+            var directive = function ($compile, variables) {
+                return new Honeydew.FesRepeat($compile, variables);
+            };
+            directive['$inject'] = ['$compile', 'IVariableRepository'];
+            return directive;
+        };
         return DirectiveFactory;
     })();
     Honeydew.DirectiveFactory = DirectiveFactory;
 })(Honeydew || (Honeydew = {}));
-
-angular.module('honeydew', [])
-    .constant('IVariableRepository', new VariableRepository())
-    //.directive('fesInit', Honeydew.DirectiveFactory.FesInit())
-    .directive('fesBindAttributes', Honeydew.DirectiveFactory.FesBindAttributes());
--- src/engine.js
+++ src/engine.js
@@ -1,155 +1,198 @@
-function VariableRepository() {
-    this.findByKey = function (key) {
-        var observers = {};
-
-        return {
-            observe: function (event, callback) {
-                if (observers[event] === undefined) {
-                    observers[event] = [];
-                }
-
-                observers[event].push(callback);
-            },
-            setAttributes: function (attributes) {
-                var i = observers['change:attributes'].length;
-                while (i--) {
-                    observers['change:attributes'][i]();
-                }
-            },
-            getAttributes: function () {
-                return {
-                    value: 10,
-                    style: "color:red;"
-                };
-            },
-            getKey: function () {
-                return "ROOT";
-            },
-            getTitle: function () {
-                return "ROOT";
-            },
-            getChildren: function () {
-                return [
-                    {
-                        observe: function (event, callback) {
-                            if (observers[event] === undefined) {
-                                observers[event] = [];
-                            }
-
-                            observers[event].push(callback);
-                        },
-                        setAttributes: function (attributes) {
-                            var i = observers['change:attributes'].length;
-                            while (i--) {
-                                observers['change:attributes'][i]();
-                            }
-                        },
-                        getAttributes: function () {
-                            return {
-                                value: 10,
-                                style: "color:red;"
-                            };
-                        },
-                        getKey: function () {
-                            return "CHILD1";
-                        },
-                        getTitle: function () {
-                            return "CHILD1";
-                        },
-                        getChildren: function () {
-                            return [];
-                        },
-                        getContext: function (query) {
-                            return {
-                                observe: function (event, callback) {
-                                    //
-                                },
-                                setAttributes: function (attributes) {
-                                    //
-                                },
-                                getAttributes: function () {
-                                    return {};
-                                },
-                                getKey: function () {
-                                    return "";
-                                },
-                                getTitle: function () {
-                                    return "";
-                                }
-                            };
-                        }
-                    },
-                    {
-                        observe: function (event, callback) {
-                            if (observers[event] === undefined) {
-                                observers[event] = [];
-                            }
-
-                            observers[event].push(callback);
-                        },
-                        setAttributes: function (attributes) {
-                            var i = observers['change:attributes'].length;
-                            while (i--) {
-                                observers['change:attributes'][i]();
-                            }
-                        },
-                        getAttributes: function () {
-                            return {
-                                value: 10,
-                                style: "color:red;"
-                            };
-                        },
-                        getKey: function () {
-                            return "CHILD2";
-                        },
-                        getTitle: function () {
-                            return "CHILD2";
-                        },
-                        getChildren: function () {
-                            return [];
-                        },
-                        getContext: function (query) {
-                            return {
-                                observe: function (event, callback) {
-                                    //
-                                },
-                                setAttributes: function (attributes) {
-                                    //
-                                },
-                                getAttributes: function () {
-                                    return {};
-                                },
-                                getKey: function () {
-                                    return "";
-                                },
-                                getTitle: function () {
-                                    return "";
-                                }
-                            };
-                        }
-                    }
-                ];
-            },
-            getContext: function (query) {
-                return {
-                    observe: function (event, callback) {
-                        //
-                    },
-                    setAttributes: function (attributes) {
-                        //
-                    },
-                    getAttributes: function () {
-                        return {};
-                    },
-                    getKey: function () {
-                        return "";
-                    },
-                    getTitle: function () {
-                        return "";
-                    }
-                };
-            }
-        };
-    };
+function VariableRepository()
+{
+	console.info('new variablerepository')
+	var datastore = {};
+	$.getJSON("V05.json", function(data)
+	{
+		console.info('loading new datastore')
+		datastore = data;
+	});
+	this.findByKey = function(key)
+	{
+		var observers = {};
+		return {
+			observe : function(event, callback)
+			{
+				if (observers[event] === undefined)
+				{
+					observers[event] = [];
+				}
+				observers[event].push(callback);
+			},
+			setAttributes : function(attributes)
+			{
+				var i = observers['change:attributes'].length;
+				while (i--)
+				{
+					observers['change:attributes'][i]();
+				}
+			},
+			getAttributes : function()
+			{
+				return {
+					value : 10,
+					style : "color:red;"
+				};
+			},
+			getKey : function()
+			{
+				return "ROOT";
+			},
+			getTitle : function()
+			{
+				return "ROOT";
+			},
+			getChildren : function()
+			{
+				return [ {
+					observe : function(event, callback)
+					{
+						if (observers[event] === undefined)
+						{
+							observers[event] = [];
+						}
+						observers[event].push(callback);
+					},
+					setAttributes : function(attributes)
+					{
+						var i = observers['change:attributes'].length;
+						while (i--)
+						{
+							observers['change:attributes'][i]();
+						}
+					},
+					getAttributes : function()
+					{
+						return {
+							value : 10,
+							style : "color:red;"
+						};
+					},
+					getKey : function()
+					{
+						return "CHILD1";
+					},
+					getTitle : function()
+					{
+						return "CHILD1";
+					},
+					getChildren : function()
+					{
+						return [];
+					},
+					getContext : function(query)
+					{
+						return {
+							observe : function(event, callback)
+							{
+								//
+							},
+							setAttributes : function(attributes)
+							{
+								//
+							},
+							getAttributes : function()
+							{
+								return {};
+							},
+							getKey : function()
+							{
+								return "";
+							},
+							getTitle : function()
+							{
+								return "";
+							}
+						};
+					}
+				}, {
+					observe : function(event, callback)
+					{
+						if (observers[event] === undefined)
+						{
+							observers[event] = [];
+						}
+						observers[event].push(callback);
+					},
+					setAttributes : function(attributes)
+					{
+						var i = observers['change:attributes'].length;
+						while (i--)
+						{
+							observers['change:attributes'][i]();
+						}
+					},
+					getAttributes : function()
+					{
+						return {
+							value : 10,
+							style : "color:red;"
+						};
+					},
+					getKey : function()
+					{
+						return "CHILD2";
+					},
+					getTitle : function()
+					{
+						return "CHILD2";
+					},
+					getChildren : function()
+					{
+						return [];
+					},
+					getContext : function(query)
+					{
+						return {
+							observe : function(event, callback)
+							{
+								//
+							},
+							setAttributes : function(attributes)
+							{
+								//
+							},
+							getAttributes : function()
+							{
+								return {};
+							},
+							getKey : function()
+							{
+								return "";
+							},
+							getTitle : function()
+							{
+								return "";
+							}
+						};
+					}
+				} ];
+			},
+			getContext : function(query)
+			{
+				return {
+					observe : function(event, callback)
+					{
+						//
+					},
+					setAttributes : function(attributes)
+					{
+						//
+					},
+					getAttributes : function()
+					{
+						return {};
+					},
+					getKey : function()
+					{
+						return "";
+					},
+					getTitle : function()
+					{
+						return "";
+					}
+				};
+			}
+		};
+	};
 }
-
