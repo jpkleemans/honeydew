87ff588379f1f15ff0e2e87458e966865bd7d78f
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-06-01 14:40:29 +0200

    Cleanup

---
+++ src/Main.ts
@@ -0,0 +1,19 @@
+declare class VariableRepository
+{
+}
+
+module Honeydew
+{
+    export class Module
+    {
+        public static main(angular)
+        {
+            return angular.module('honeydew', [])
+                .constant('IVariableRepository', new VariableRepository())
+                .directive('fesBindAttributes', Honeydew.DirectiveFactory.createFesBindAttributes())
+                .directive('fesRepeat', Honeydew.DirectiveFactory.createFesRepeat());
+        }
+    }
+}
+
+Honeydew.Module.main(angular);
