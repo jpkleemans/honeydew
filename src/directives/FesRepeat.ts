2897a634df2a7df5278f0f5e09adc604ecb33a40
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-06-03 13:41:24 +0200

    Use pre

--- src/directives/FesRepeat.ts
+++ src/directives/FesRepeat.ts
@@ -8,7 +8,7 @@ module Honeydew
         /**
          * Modify the DOM
          */
-        public link;
+        public compile;
 
         /**
          * FES VariableRepository
@@ -34,34 +34,39 @@ module Honeydew
             this.variables = variables;
             this.variableInitializer = variableInitializer;
 
-            this.link = (scope:angular.IScope, element:angular.IAugmentedJQuery, attrs:angular.IAttributes) =>
+            this.compile = () =>
             {
-                var expression = attrs['fesRepeat'];
-                var key = expression.match(new RegExp("in (\\S[^.\\s]*)(?:.*)$"))[1];
-                var property = expression.match(new RegExp("in (?:\\S[^.]*).(\\S*)(?:.*)$"))[1];
+                return {
+                    pre: (scope:angular.IScope, element:angular.IAugmentedJQuery, attrs:angular.IAttributes) =>
+                    {
+                        var expression = attrs['fesRepeat'];
+                        var key = expression.match(new RegExp("in (\\S[^.\\s]*)(?:.*)$"))[1];
+                        var property = expression.match(new RegExp("in (?:\\S[^.]*).(\\S*)(?:.*)$"))[1];
 
-                if (scope[key] === undefined) {
-                    this.variableInitializer.init(key, scope);
-                }
+                        if (scope[key] === undefined) {
+                            this.variableInitializer.init(key, scope);
+                        }
 
-                switch (property) {
-                    case 'children':
-                        var children = scope[key].variable.getChildren();
-                        var uiChildren = this.variableInitializer.createUIChildren(children);
-                        scope[key].children = uiChildren;
-                        break;
-                    case 'contexts':
-                        var query = attrs['fesContextQuery'];
-                        var contexts = scope[key].variable.getContexts(query);
-                        var uiContexts = this.variableInitializer.createUIContexts(contexts);
-                        scope[key].contexts = uiContexts;
-                        break;
-                }
+                        switch (property) {
+                            case 'children':
+                                var children = scope[key].variable.getChildren();
+                                var uiChildren = this.variableInitializer.createUIChildren(children);
+                                scope[key].children = uiChildren;
+                                break;
+                            case 'contexts':
+                                var query = attrs['fesContextQuery'];
+                                var contexts = scope[key].variable.getContexts(query);
+                                var uiContexts = this.variableInitializer.createUIContexts(contexts);
+                                scope[key].contexts = uiContexts;
+                                break;
+                        }
 
-                element.attr('ng-repeat', expression);
+                        element.attr('ng-repeat', expression);
 
-                element.removeAttr('fes-repeat');
-                $compile(element)(scope);
+                        element.removeAttr('fes-repeat');
+                        $compile(element)(scope);
+                    }
+                };
             }
         }
     }
