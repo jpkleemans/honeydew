dcf5459fd889e5fb9234e4a19283703807a79ad9
Michael Bakker <michael.bakker@finan.nl>
2015-06-11 15:46:18 +0200

    return proto

--- build/EngineAdapter.js
+++ build/EngineAdapter.js
@@ -6,133 +6,136 @@
 var cacheVars = {};
 function updateAll()
 {
-	for ( var variableName in cacheVars)
-	{
-		cacheVars[variableName].update();
-	}
+    for (var variableName in cacheVars)
+    {
+        cacheVars[variableName].update();
+    }
 }
 function templateContext(context, variable, query)
 {
-	var prototype = {
-		key : query.columnidx,
-		title : varname,
-		attributes : {},
-		setAttributes : function(attributes)
-		{
-			console.info('templateContext change attributes' + JSON.stringify(attributes))
-			if (variable !== undefined)
-			{
-				variable.setValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[query.timelineidx][query.columnidx], parseFloat(attributes.value));
-			}
-			this.attributes = attributes;
-			updateAll();
-		},
-		update : function()
-		{
-			this.attributes.value = variable == undefined ? 0 : variable.getValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[query.timelineidx][query.columnidx]);
-			this.attributes.style = {
-				color : 'green'
-			}
-		}
-	};
-	prototype.update();
-	return prototype;
+    var prototype = {
+        key: query.columnidx,
+        title: varname,
+        attributes: {},
+        setAttributes: function (attributes)
+        {
+            console.info('templateContext change attributes' + JSON.stringify(attributes))
+            if (variable !== undefined)
+            {
+                variable.setValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[query.timelineidx][query.columnidx], parseFloat(attributes.value));
+            }
+            this.attributes = attributes;
+            updateAll();
+        },
+        update: function ()
+        {
+            this.attributes.value = variable == undefined ? 0 : variable.getValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[query.timelineidx][query.columnidx]);
+            this.attributes.style = {
+                color: 'green'
+            }
+        }
+    };
+    prototype.update();
+    return prototype;
 }
 function VariableRepository()
 {
-	// variabeltje gaat waarschijnlijk via constructor oid
-	var context = {
-		maxChildVariables : 600
-	};
-	var dummqueries = [ {
-		timelineidx : 0,
-		columnidx : 1
-	}, {
-		timelineidx : 0,
-		columnidx : 2
-	} ];
-	console.info('new VariableRepository constructed, expecting once during web-app lifecycle.')
-	// we aqquire are a new Model instance from the json template, once resolved we inject it into the Service wrapper.
-	var v05Instance = json['v05instance'];
-	var userFormulas = json['defaultmath'];
-	var importData = json['v05baseimportinstance'];
-	var v05layout = json['v05layout'];
-	// Retrieve user-formula's.
-	// FormulaBootstrap is responsible for parsing the String formula's into javascript functions
-	context.modelBuilder = new FormulaBootstrap(v05Instance, userFormulas);
-	// the CalculationModel is the DataStore, which makes modifications on the Document which is given in constructor
-	context.activeModel = new CalculationModel(v05Instance);
-	context.calcDocument = new CalculationDocument(importData);
-	// have to do something with the parent/child relation here too
-	// from here the one and only IVariableRepository Interface function was exposed, which is am very happy with.
-	function templateVariable(varname)
-	{
-		var variable = context.activeModel[varname];
-		// except the functions a node has to support, this is quite many, but possible
-		var prototype = {
-			key : varname,
-			title : varname,
-			attributes : {},
-			children : [],
-			contexts : [],
-			setAttributes : function(attributes)
-			{
-				console.info('templateVariable change attributes' + JSON.stringify(attributes))
-				if (variable !== undefined)
-				{
-					variable.setValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[0][0], parseFloat(attributes.value));
-				}
-				this.attributes = attributes;
-				updateAll();
-			},
-			getContexts : function(query)
-			{
-				console.info('getContexts called with query ' + query)
-				this.contexts = [ templateContext(context, variable, dummqueries[0]), templateContext(context, variable, dummqueries[1]) ];
-			},
-			update : function()
-			{
-				this.attributes.value = variable == undefined ? 0 : variable.getValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[0][0]);
-				this.attributes.style = {
-					color : 'red'
-				}
-				this.contexts.forEach(function(elem)
-				{
-					elem.update();
-				});
-			}
-		};
-		prototype.update();
-	}
-	function proxyChildren(tVar, parentChildname)
-	{
-		return function()
-		{
-			var variable = context.activeModel[parentChildname];
-			var childs = [];
-			if (variable !== undefined && v05layout[parentChildname] !== undefined)
-			{
-				for ( var childname in v05layout[parentChildname])
-				{
-					var childVariable = templateVariable(childname);
-					childVariable.getChildren = proxyChildren(childVariable, childname);
-					childs.push(childVariable);
-				}
-			}
-			console.info('called children for ' + parentChildname + ' returned ' + childs.length)
-			tVar.children = childs;
-		};
-	}
-	// from here the one and only IVariableRepository Interface function was exposed, which is am very happy with.
-	this.findByKey = function(key, scope)
-	{
-		var tVar = cacheVars[key];
-		if (tVar == undefined)
-		{
-			tVar = templateVariable(key);
-			tVar.expandChildren = proxyChildren(tVar, key);
-			cacheVars[key] = tVar;
-		}
-		return tVar;
-	};
+    // variabeltje gaat waarschijnlijk via constructor oid
+    var context = {
+        maxChildVariables: 600
+    };
+    var dummqueries = [{
+        timelineidx: 0,
+        columnidx: 1
+    }, {
+        timelineidx: 0,
+        columnidx: 2
+    }];
+    console.info('new VariableRepository constructed, expecting once during web-app lifecycle.')
+    // we aqquire are a new Model instance from the json template, once resolved we inject it into the Service wrapper.
+    var v05Instance = json['v05instance'];
+    var userFormulas = json['defaultmath'];
+    var importData = json['v05baseimportinstance'];
+    var v05layout = json['v05layout'];
+    // Retrieve user-formula's.
+    // FormulaBootstrap is responsible for parsing the String formula's into javascript functions
+    context.modelBuilder = new FormulaBootstrap(v05Instance, userFormulas);
+    // the CalculationModel is the DataStore, which makes modifications on the Document which is given in constructor
+    context.activeModel = new CalculationModel(v05Instance);
+    context.calcDocument = new CalculationDocument(importData);
+    // have to do something with the parent/child relation here too
+    // from here the one and only IVariableRepository Interface function was exposed, which is am very happy with.
+    function templateVariable(varname)
+    {
+        var variable = context.activeModel[varname];
+        // except the functions a node has to support, this is quite many, but possible
+        var prototype = {
+            key: varname,
+            title: varname,
+            attributes: {},
+            children: [],
+            contexts: [],
+            setAttributes: function (attributes)
+            {
+                console.info('templateVariable change attributes' + JSON.stringify(attributes))
+                if (variable !== undefined)
+                {
+                    variable.setValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[0][0], parseFloat(attributes.value));
+                }
+                this.attributes = attributes;
+                updateAll();
+            },
+            getContexts: function (query)
+            {
+                console.info('getContexts called with query ' + query)
+                this.contexts = [templateContext(context, variable, dummqueries[0]), templateContext(context, variable, dummqueries[1])];
+            },
+            update: function ()
+            {
+                this.attributes.value = variable == undefined ? 0 : variable.getValue(variable.hIndex[0], 0, context.calcDocument.viewmodes.detl.columns[0][0]);
+                this.attributes.style = {
+                    color: 'red'
+                }
+                this.contexts.forEach(function (elem)
+                {
+                    elem.update();
+                });
+            }
+        };
+        prototype.update();
+        return prototype;
+    }
+
+    function proxyChildren(tVar, parentChildname)
+    {
+        return function ()
+        {
+            var variable = context.activeModel[parentChildname];
+            var childs = [];
+            if (variable !== undefined && v05layout[parentChildname] !== undefined)
+            {
+                for (var childname in v05layout[parentChildname])
+                {
+                    var childVariable = templateVariable(childname);
+                    childVariable.getChildren = proxyChildren(childVariable, childname);
+                    childs.push(childVariable);
+                }
+            }
+            console.info('called children for ' + parentChildname + ' returned ' + childs.length)
+            tVar.children = childs;
+        };
+    }
+
+    // from here the one and only IVariableRepository Interface function was exposed, which is am very happy with.
+    this.findByKey = function (key, scope)
+    {
+        var tVar = cacheVars[key];
+        if (tVar === undefined)
+        {
+            tVar = templateVariable(key);
+            tVar.expandChildren = proxyChildren(tVar, key);
+            cacheVars[key] = tVar;
+        }
+        return tVar;
+    };
 }
