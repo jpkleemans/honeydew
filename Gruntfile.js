ee1aa03ba18420962da7683fd30ee87204069a9d
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-06-12 12:26:43 +0200

    folder structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt)
 						return filename.toLowerCase();
 					}
 				},
-				src : [ 'test/json/*.json' ],
+				src : [ 'json/*.json' ],
 				dest : 'build/json.js'
 			}
 		}
