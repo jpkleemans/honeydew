e0b5981e42cc10bc67b25ba7f80ffee14f23e936
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-06-12 13:41:54 +0200

    Dynamically add children

--- src/controllers/FesController.ts
+++ src/controllers/FesController.ts
@@ -3,12 +3,25 @@
  * Created by bernard on 12-6-2015.
  */
 
-module Honeydew {
-    export class FesController {
+module Honeydew
+{
+    export class FesController
+    {
         public columnQuery:any;
 
-        constructor($scope:angular.IScope) {
+        constructor($scope:angular.IScope)
+        {
             $scope['columnQuery'] = '{"timeline": 0, "start": 0, "end": 4}';
+
+            $scope['addChild'] = (variable) =>
+            {
+                variable.children.push({
+                    attributes: {
+                        value: 1000
+                    },
+                    children:[]
+                });
+            }
         }
     }
 }
