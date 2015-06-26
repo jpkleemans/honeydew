1b40951c846e266c9c8ded7f795ee4471935ac30
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-06-26 14:10:46 +0200

    ContextRepositorySpec

--- spec/repositories/ContextRepositorySpec.ts
+++ spec/repositories/ContextRepositorySpec.ts
@@ -1,7 +1,17 @@
 /// <reference path="../../type_definitions/jasmine/jasmine.d.ts" />
 /// <reference path="../../src/repositories/IContextRepository.ts" />
 
-declare var json;
+declare var testjson;
+
+declare class CalculationDocument
+{
+    constructor(var1);
+}
+
+declare class ContextRepository
+{
+    constructor(var1, var2);
+}
 
 module Honeydew.Spec
 {
@@ -10,25 +20,43 @@ module Honeydew.Spec
         // SUT
         var contextRepo:IContextRepository;
 
-        // Mocks
-        //
-
         beforeEach(() =>
         {
-            //var v05instance = testjson['v05instance'];
-            //var v05layout = testjson['v05layout'];
-            //
-            //contextRepo = new ContextRepository();
+            var importData = testjson['v05baseimportinstance'];
+            var v05Instance = testjson['v05instance'];
+            var CalculationModel = {};
+            CalculationModel['Q_ROOT_VARIABLE_ONE'] = jasmine.createSpyObj('CalculationModel', ['getValue', 'setValue']);
+            CalculationModel['Q_ROOT_VARIABLE_ONE'].hIndex = [];
+            CalculationModel['Q_ROOT_VARIABLE_ONE'].hIndex[0] = null;
+
+            var CalculationDocument = jasmine.createSpyObj('CalculationDocument', ['getValue', 'setValue']);
+            CalculationDocument.viewmodes = {};
+            CalculationDocument.viewmodes.det1 = {};
+            CalculationDocument.viewmodes.det1.columns = [];
+            CalculationDocument.viewmodes.det1.columns[0] = [{}, {}, {}, {}, {}, {}, {}, {}];
+            contextRepo = new ContextRepository(CalculationDocument, CalculationModel);
         });
 
         it("finds contexts by a query", () =>
         {
-            // ¯\_(ツ)_/¯
+            var testquery = {
+                "timeline": 0,
+                "start": 0,
+                "end": 4,
+                "variableKey": "Q_ROOT_VARIABLE_ONE"
+            };
+            expect(testquery.end).toEqual(4);
         });
 
         it("finds the first context", () =>
         {
-            // ¯\_(ツ)_/¯
+            var testquery = {
+                "timeline": 0,
+                "start": 0,
+                "end": 4,
+                "variableKey": "Q_ROOT_VARIABLE_ONE"
+            };
+            expect(testquery.start).toEqual(0);
         });
     });
 }
